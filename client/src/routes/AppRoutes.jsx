de0915cfaa7140eecd0a0cf7d6c525a3884470f8
Dheeraj-cre <Dheerajn2311@gmail.com>
2026-06-26 10:56:58 +0530

    fixrout

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,65 +1,21 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
 import Dashboard from "../pages/Dashboard/Dashboard";
+
+import ProtectedRoute from "../components/ProtectedRoute";
 import Upload from "../pages/Upload/Upload";
 import History from "../pages/History/History";
 import Itinerary from "../pages/Itinerary/Itinerary";
 
-import ProtectedRoute from "../components/ProtectedRoute";
-
 export default function AppRoutes() {
-  const token = localStorage.getItem("token");
-
   return (
     <BrowserRouter>
       <Routes>
 
-        {/* Default Route */}
         <Route
           path="/"
-          element={
-            token ? (
-              <Navigate to="/dashboard" replace />
-            ) : (
-              <Navigate to="/login" replace />
-            )
-          }
-        />
-
-        {/* Public Routes */}
-        <Route
-          path="/login"
-          element={
-            token ? (
-              <Navigate to="/dashboard" replace />
-            ) : (
-              <Login />
-            )
-          }
-        />
-
-        <Route
-          path="/register"
-          element={
-            token ? (
-              <Navigate to="/dashboard" replace />
-            ) : (
-              <Register />
-            )
-          }
-        />
-
-        {/* Protected Routes */}
-
-        <Route
-          path="/dashboard"
           element={
             <ProtectedRoute>
               <Dashboard />
@@ -68,44 +24,39 @@ export default function AppRoutes() {
         />
 
         <Route
-          path="/upload"
-          element={
-            <ProtectedRoute>
-              <Upload />
-            </ProtectedRoute>
-          }
+          path="/login"
+          element={<Login />}
         />
 
         <Route
-          path="/history"
-          element={
-            <ProtectedRoute>
-              <History />
-            </ProtectedRoute>
-          }
+          path="/register"
+          element={<Register />}
         />
-
         <Route
-          path="/itinerary/:id"
-          element={
-            <ProtectedRoute>
-              <Itinerary />
-            </ProtectedRoute>
-          }
-        />
+  path="/upload"
+  element={
+    <ProtectedRoute>
+      <Upload />
+    </ProtectedRoute>
+  }
+/>
+<Route
+  path="/history"
+  element={
+    <ProtectedRoute>
+      <History />
+    </ProtectedRoute>
+  }
+/>
+<Route
+path="/itinerary/:id"
+element={
+<ProtectedRoute>
+<Itinerary/>
+</ProtectedRoute>
+}
+/>
 
-        {/* 404 Route */}
-
-        <Route
-          path="*"
-          element={
-            token ? (
-              <Navigate to="/dashboard" replace />
-            ) : (
-              <Navigate to="/login" replace />
-            )
-          }
-        />
       </Routes>
     </BrowserRouter>
   );
