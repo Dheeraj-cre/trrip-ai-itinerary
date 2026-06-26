54ebb77bf5780341d27c3e3b305d5f0994fe7ff0
Dheeraj-cre <Dheerajn2311@gmail.com>
2026-06-26 10:19:50 +0530

    app.js

--- server/app.js
+++ server/app.js
@@ -1,39 +1,64 @@
+
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-import errorHandler from "./middleware/errorMiddleware.js";
+
 import authRoutes from "./routes/authRoutes.js";
 import testRoutes from "./routes/testRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
-import path from "path";
 import dashboardRoutes from "./routes/dashboardRoutes.js";
 import itineraryRoutes from "./routes/itineraryRoutes.js";
 
-
-
-
+import errorHandler from "./middleware/errorMiddleware.js";
 
 const app = express();
 
-app.use(cors());
+/* -----------------------------
+   Middlewares
+------------------------------ */
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://trrip-ai-itinerary.vercel.app/login",
+    ],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(morgan("dev"));
-app.use(errorHandler);
+
+/* -----------------------------
+   Static Files
+------------------------------ */
+
+app.use("/uploads", express.static("uploads"));
+
+/* -----------------------------
+   Routes
+------------------------------ */
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Welcome to Trrip AI API ",
+  });
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/test", testRoutes);
 app.use("/api/upload", uploadRoutes);
-app.use("/uploads", express.static("uploads"));
 app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/itinerary", itineraryRoutes);
 
-app.use(express.urlencoded({ extended: true }));
+/* -----------------------------
+   Error Handler (Always Last)
+------------------------------ */
 
-app.get("/", (req, res) => {
-    res.json({
-        success: true,
-        message: "Welcome to Trrip AI API"
-    });
-});
+app.use(errorHandler);
 
 export default app;
